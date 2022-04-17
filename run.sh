6fecfc0acf34151aac7aaecb1190542fbc1b6bb3
Hediberto Cavalcante da Silva <hed.cavalcante@gmail.com>
2022-04-17 20:33:17 -0300

    CRLF run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Use this script only in a development server.
-
 if [ -d "venv" ]
 then
     . venv/bin/activate
@@ -8,14 +7,10 @@ else
     python3 -m venv venv
     . venv/bin/activate
 fi
-
 pip install .
-
 export FLASK_DEBUG=False
 export FLASK_APP=wsgi.py
 export ADMIN_USER=admin
 export ADMIN_PASSWORD=admin
-
 PUBLISHED_PORT=5000
-
 gunicorn --bind 0.0.0.0:$PUBLISHED_PORT  wsgi:app --capture-output --enable-stdio-inheritance
